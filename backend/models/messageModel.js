1868975c13e8a4c765d544f6c62db5e8ecf83af3
shivampatidar887 <110610536+shivampatidar887@users.noreply.github.com>
2023-12-04 09:57:06 +0530

    second commit

--- backend/models/messageModel.js
+++ backend/models/messageModel.js
@@ -5,10 +5,18 @@ const messageSchema = mongoose.Schema(
     sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     content: { type: String, trim: true },
     chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
-    isRead: [],
+    readBy: { type: Object, default: {} },
   },
   { timestamps: true }
 );
+
+// Custom method to mark a message as read by a specific user
+messageSchema.methods.markAsReadByUser = async function (userId) {
+  if (!this.readBy[userId]) {
+    this.readBy[userId] = true;
+    await this.save();
+  }
+};
 
 const Message = mongoose.model("Message", messageSchema);
 module.exports = Message;
--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,17 @@ const path = require('path');
 const dotenv = require('dotenv');
 const connectDB = require("./config/db");
 const socketIO = require('socket.io');
-const {chats} =require("./data/data");
+const messageRoutes = require('./Routes/messageRoutes')
+// const { chats } = require("./data/data");
 
 const app = express();
-dotenv.config({path:"backend/config/.env"});
+dotenv.config({ path: "backend/config/.env" });
 connectDB();
 const server = http.createServer(app);
-// const io = socketIO(server);
+const io = socketIO(server);
+
+// Body parsing middleware
+app.use(express.json());
 
 // app.use(express.static(path.resolve('./public')));
 
@@ -19,25 +23,35 @@ const server = http.createServer(app);
 //     return res.sendFile(path.join(__dirname, '/public/index.html'));
 // });
 
-app.get("/api/chats",(req,res)=>{
+app.get("/api/chats", (req, res) => {
   res.send(chats)
 })
-app.get("/api/chat/:id",(req,res)=>{
-  const singlechat=chats.find((c)=>c._id === req.params.id);
+app.get("/api/chat/:id", (req, res) => {
+  const singlechat = chats.find((c) => c._id === req.params.id);
   res.send(singlechat);
 })
+app.use('/api/message',messageRoutes);
 // Socket.IO configuration
-// io.on('connection', (socket) => {
-//   console.log('A new user has connected  jhjhhhhhuh');
+io.on('connection', (socket) => {
+  console.log('A new user has connected');
 
-//   socket.on("user-message",(message)=>{
-//     io.emit("message",message);
-//   });
+  socket.on('messageRead', async (messageId, userId) => {
+    // Update the read status in the database
+    const message = await Message.findById(messageId);
+    if (message) {
+      await message.markAsReadByUser(userId);
+      io.emit('messageRead', { messageId, userId });
+    }
+  });
 
-//   socket.on('disconnect', () => {
-//     console.log('User disconnected');
-//   });
-// });
+  socket.on("user-message", (message) => {
+    io.emit("message", message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
 
 // Start the server
 const PORT = process.env.PORT || 8000;
